bc8cb35cf2044ba708600e9dc8e5e3be21a068d7
ButkiewiczP <butkiewicz.p@gmail.com>
2012-08-14 12:26:40 -0300

    Update example.php

--- example.php
+++ example.php
@@ -1,15 +1,15 @@
 <?php
-	require_once('ean.class.new.php');
+    require_once('ean.class.new.php');
 
-	$search = new EAN_API();
+    $search = new EAN_API();
 	
-  	$infoArray['city'] = "San Diego";
+    $infoArray['city'] = "San Diego";
     $infoArray['checkIn'] = "05/08/2013";
     $infoArray['checkOut'] = "05/13/2013";
-	$infoArray['numberOfRooms'] = 1;
-	$infoArray['room-0-adult-total'] = 2;
-	$infoArray['room-0-child-total'] = 0;
-		
-	$result = $search->getHotels($infoArray);
-	print_r($result);
+    $infoArray['numberOfRooms'] = 1;
+    $infoArray['room-0-adult-total'] = 2;
+    $infoArray['room-0-child-total'] = 0;
+	
+    $result = $search->getHotels($infoArray);
+    print_r($result);
 ?>
